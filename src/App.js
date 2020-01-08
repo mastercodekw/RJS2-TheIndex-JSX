47d4f5190c06bc4b4706ca029c2a430750c7ea4e
Mohammed ALSarraf <mastercodekw@gmail.com>
2020-01-08 19:02:41 +0300

    Removes all console errors.

--- src/App.js
+++ src/App.js
@@ -3,39 +3,42 @@ import authors from "./data";
 
 function App() {
   return (
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-2">
+    <div id="app" className="container-fluid">
+      <div className="row">
+        <div className="col-2">
           <div id="sidebar">
-            <img src="theindex.svg" class="logo" alt="the index logo" />
+            <img src="theindex.svg" className="logo" alt="the index logo" />
             <section>
-              <h4 class="menu-item active">
+              <h4 className="menu-item active">
                 <button>AUTHORS</button>
               </h4>
             </section>
           </div>
         </div>
-        <div class="content col-10">
-          <div class="authors">
+        <div className="content col-10">
+          <div className="authors">
             <h3>Authors</h3>
-            <div class="row">
+            <div className="row">
               {authors.map(author => (
-                <div class="col-lg-4 col-md-6 col-12">
-                  <div class="card">
-                    <div class="image">
+                <div
+                  className="col-lg-4 col-md-6 col-12"
+                  key={author.first_name + " " + author.last_name}
+                >
+                  <div className="card">
+                    <div className="image">
                       <img
-                        class="card-img-top img-fluid"
+                        className="card-img-top img-fluid"
                         src={author.imageUrl}
                         alt={author.first_name + " " + author.last_name}
                       />
                     </div>
-                    <div class="card-body">
-                      <h5 class="card-title">
+                    <div className="card-body">
+                      <h5 className="card-title">
                         <span>
                           {author.first_name} {author.last_name}
                         </span>
                       </h5>
-                      <small class="card-text">
+                      <small className="card-text">
                         {author.books.length} books
                       </small>
                     </div>
